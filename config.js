5e3da9291a5a93ae305952898f7b4787fe142d15
sincero319 <ignaciohdezuasd@gmail.com>
2026-06-17 11:02:24 -0400

    Change API key in config.js
    Updated API key in chatbot configuration.

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ window.CHATBOT_CONFIG = {
   provider: "groq",
 
   // Pega aqui tu API key
-  apiKey: "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"
+  apiKey: "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"
 };
